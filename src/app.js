17a6f28db57ed0e2d25f998beff3aa96ab33d210
JosefaMiller <josefamillerb@gmail.com>
2023-02-28 05:11:20 +0000

    done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominio = [".cl", ".com", ".es", ".net", ".org"];
+  for (let Randompro = 0; Randompro < pronoun.length; Randompro++) {
+    for (let Randomadj = 0; Randomadj < adj.length; Randomadj++) {
+      for (let Randomnoun = 0; Randomnoun < noun.length; Randomnoun++) {
+        for (let Randomdom = 0; Randomdom < dominio.length; Randomdom++) {
+          console.log(
+            pronoun[Randompro] +
+              adj[Randomadj] +
+              noun[Randomnoun] +
+              dominio[Randomdom]
+          );
+        }
+      }
+    }
+  }
 };
